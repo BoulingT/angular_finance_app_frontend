d66147ab3c5db53c121541fa05acba1974557696
Bouling <theo.boulinguez@le-campus-numerique.fr>
2024-08-21 16:02:00 +0200

    update authentication controller path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api', // Replace with your development API URL
+  apiUrl: 'http://localhost:8080/api/authentication', // Replace with your development API URL
 };
